bfdecafadb0ce07884ab16be3cf5dc13d9722ce4
PetrDidur <pdidur@yandex.ru>
2023-03-25 12:27:15 +0300

    second function

--- funcs.py
+++ funcs.py
@@ -1,3 +1,6 @@
 def sum_(a, b):
     print(a + b)
 
+
+def subtraction(a, b):
+    print(a - b)
--- main.py
+++ main.py
@@ -1,2 +1,4 @@
-from funcs import sum_
+from funcs import sum_, subtraction
 sum_(1, 3)
+
+subtraction(2, 1)
